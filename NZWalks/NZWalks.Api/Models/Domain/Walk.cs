35baf6b3ad2a44594f5f426bc1fca87544311326
tridip25 <tridipsharma25@gmail.com>
2022-07-27 22:07:54 +0530

    END OF SECTION 2

---
+++ NZWalks/NZWalks.Api/Models/Domain/Walk.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks.Api.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        
+        //Navigation Properties
+
+        //connecting back to region
+        public Region region { get; set; }
+
+        //connecting to walkDifficulty
+        public WalkDifficulty WalkDifficulty { get; set; } // this got [1:1] cardinality
+    }
+}
